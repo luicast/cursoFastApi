83f99a37a72a5f6296eb70a37c925de47e2c505c
luicast <luis_castilla@live.com>
2022-06-27 11:08:46 -0500

    body

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+#python
+from typing import Optional
+
+#pydantic
+from pydantic import BaseModel
+
+#FAstAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def Home():
     return {"message": "Hello World"}
+
+    #request and response Body
+
+@app.post("/person/new")
+def createPerson(person: Person = Body(...)):
+    return person
